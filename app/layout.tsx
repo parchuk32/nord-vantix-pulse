928bc0bda76469bdaddd7b7d0962c36fca42f409
Nord.Vantix <tristanpalamarchuk@gmail.com>
2026-04-11 22:34:45 -0400

    Dark Animated Landing Page: Twitch-style focus

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Share_Tech_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header"; // 1. Importe ton nouveau Header ici
+import Header from "./components/Header"; // 1. Importe ton nouveau Header ici
 
 const shareTechMono = Share_Tech_Mono({
   weight: "400",
